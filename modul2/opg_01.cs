b1324ac57e34d0c4b07c24b938fd2bf71430ea80
rosell <Gustavrosell@gmail.com>
2023-08-31 11:34:29 +0200

    hehe siu

--- modul2/opg_01.cs
+++ modul2/opg_01.cs
@@ -1,32 +1,46 @@
-using System;
-
-class opg_01
+public class opg_01
 {
     public void Run()
     {
-        Console.WriteLine("Enter the amount: ");
+        Console.WriteLine("Indtast mængde: ");
         int amount = Convert.ToInt32(Console.ReadLine());
-        
-        int stamps3 = Stamps3(amount);
-        int stamps5 = Stamps5(amount);
 
-        Console.WriteLine($"\n3-cent stamps needed: {stamps3}");
-        Console.WriteLine($"\n5-cent stamps needed: {stamps5}");
+        if (amount >= 8)
+        {
+            int[] result = CalculateStamps(amount);
+
+            Console.WriteLine($"Antal 5 cents frimærker: {result[0]}");
+            Console.WriteLine($"Antal 3 cents frimærker: {result[1]}");
+        }
+        else
+        {
+            Console.WriteLine("Beløbet skal være mindst 8 cent for at beregne frimærker.");
+        }
     }
 
-    // Calculate the number of 3-cent stamps needed to frank the given amount
-    // amount > 7
-    // Returns the number of 3-cent stamps
-    static int Stamps3(int amount)
+    public int[] CalculateStamps(int amount)
     {
-        return amount / 3;
+        int[] stamps = new int[2]; // Index 0: Antal 5 cents frimærker, Index 1: Antal 3 cents frimærker
+
+        int maxStamps5 = amount / 5; // Maksimalt antal 5 cents frimærker uden rest
+
+        for (int s5 = maxStamps5; s5 >= 0; s5--)
+        {
+            int remainingAmount = amount - (s5 * 5);
+
+            if (remainingAmount % 3 == 0)
+            {
+                int stamps5 = s5;
+                int stamps3 = remainingAmount / 3;
+
+                stamps[0] = stamps5;
+                stamps[1] = stamps3;
+
+                return stamps;
+            }
+        }
+
+        return stamps; // return [0, 0], hvis intet er fundet
     }
 
-    // Calculate the number of 5-cent stamps needed to frank the given amount
-    // amount > 7
-    // Returns the number of 5-cent stamps
-    static int Stamps5(int amount)
-    {
-        return amount / 5;
-    }
 }
---
+++ modul2/opg_02.cs
@@ -0,0 +1,13 @@
+﻿
+public class opg_02
+{
+    public void Run()
+    {
+
+        //Returnerer sand hvis s er et palindrom
+        bool IsPalindrome(string s) {
+        }
+
+    }
+}
+
