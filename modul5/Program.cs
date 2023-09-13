094df4ae84e4da4ae104da43388d44c35c9120a9
Rosell22V <Gustav@klitholm.com>
2023-09-13 15:27:04 +0200

    ffs

--- modul5/Program.cs
+++ modul5/Program.cs
@@ -1,20 +1,69 @@
-﻿class Program
+﻿
+class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        // Opretter et DiceCup objekt med to terninger
-        DiceCup cup = new DiceCup();
+        new Opgave5_1version2().Run();
 
-        // Kaster terningerne og tilføjer resultatet til historikken
-        cup.Roll();
+    }
 
-        // Udskriver historikken
-        foreach (var rolls in cup.History)
+    static void TryPlayWithList()
+    {
+        Console.WriteLine("Hello, World!");
+        List<string> names = new List<string>
         {
-            Console.WriteLine(string.Join(", ", rolls));
-        }
+            "Bruce",
+            "Alfred",
+            "Tim",
+            "Richard"
+        };
 
-        // Nulstiller historikken
-        cup.ResetHistory();
+        names.ForEach(name => Console.WriteLine(name));
+
+        names.Add("Peter");
+        names.Add("Jens");
+
+        Console.WriteLine("[" + string.Join(",", names) + "]");
+
+        Console.WriteLine("Og dem med et e i eller som ender på d");
+        names = names.FindAll(name => name.Contains('e') && name.EndsWith('d'));
+        names.ForEach(name => Console.WriteLine(name));
+
+        //Console.WriteLine(names.Aggregate((x, y) => x + ", " + y));
+    }
+
+    static void TryPlayWithRemove()
+    {
+        Console.WriteLine("Hello, World!");
+        List<string> names = new List<string>
+        {
+            "Bruce",
+            "Alfred",
+            "Tim",
+            "Richard"
+        };
+
+        names.ForEach(name => Console.WriteLine(name));
+
+        names.Add("Peter");
+        names.Add("Jens");
+
+        Console.WriteLine("[" + string.Join(",", names) + "]");
+
+        names.Remove("tim");
+
+        Console.WriteLine("Her er Tim fjernet");
+        Console.WriteLine("[" + string.Join(",", names) + "]");
+
+
+        names.RemoveAt(1);
+        Console.WriteLine("Her er streng med index 1 fjernet");
+        Console.WriteLine("[" + string.Join(",", names) + "]");
+
+        names.RemoveAll(name => name.Contains('e'));
+        Console.WriteLine("Her er alle med et e i fjernet");
+        Console.WriteLine("[" + string.Join(",", names) + "]");
+
+        //Console.WriteLine(names.Aggregate((x, y) => x + ", " + y));
     }
 }
